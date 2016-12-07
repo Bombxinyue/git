de24cef421f92aa82c535b5c3ec388142b7e3deb
pengxinyue <504193487@qq.com>
2016-12-07 22:25:48 +0800

    彭欣月12.7上传

--- app/src/main/java/com/example/myapplication6/FActivity.java
+++ app/src/main/java/com/example/myapplication6/FActivity.java
@@ -1,7 +1,15 @@
 package com.example.myapplication6;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by 彭欣月 on 2016/11/25.
  */
-public class FActivity {
+public class FActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
 }
--- app/src/main/java/com/example/myapplication6/MainActivity.java
+++ app/src/main/java/com/example/myapplication6/MainActivity.java
@@ -1,20 +1,22 @@
 package com.example.myapplication6;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
 
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
             setContentView(R.layout.login_layout);
 
             Button button1=(Button) findViewById(R.id.login);
